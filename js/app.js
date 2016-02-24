c2fb3c03f867766b0cdfa67ba70cf057417728e9
wupa9 <wupaz9@gmail.com>
2016-02-24 15:31:51 -0600

    Setup basic routes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,42 @@
 "use strict";
 
-// Si ven un error en la consola del browser es debido a restangular (ni idea de porque)
-angular.module("VinculacionApp", ['ui.router', 'ngAnimate', 'restangular']);
+var app = angular.module("VinculacionApp", ['ui.router', 'ngAnimate']);
+
+app.config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/login');
+
+	// Por el primer sprint solo las primeras tres rutas
+	// estaran disponibles para los alumnos.
+
+	$stateProvider
+
+		.state('login', {
+			url: '/login',
+			templateUrl: '../templates/login.html'
+		})
+
+		.state('registro', {
+			url: '/registro',
+			templateUrl: '../templates/registro.html'
+		})
+
+		.state('home', {
+			url: '/home',
+			templateUrl: '../templates/home.html'
+		})
+
+		.state('home.clases', {
+			url: '/clases',
+			templateUrl: '../templates/clases.html'
+		})
+
+		.state('home.proyectos', {
+			url: '/proyectos',
+			templateUrl: '../templates/proyectos.html'
+		})
+
+		.state('home.proyecto', {
+			url: '/proyecto',
+			templateUrl: '../templates/proyecto.html'
+		});
+}]);
