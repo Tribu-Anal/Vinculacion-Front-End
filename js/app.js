7cf22c50112c99caa6d5c142580e8edb4b40e467
wupa9 <wupaz9@gmail.com>
2016-02-24 02:42:14 -0600

    Project setup for first sprint

---
+++ js/app.js
@@ -0,0 +1,4 @@
+"use strict";
+
+// Si ven un error en la consola del browser es debido a restangular (ni idea de porque)
+angular.module("VinculacionApp", ['ui.router', 'ngAnimate', 'restangular']);
