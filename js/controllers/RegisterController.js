2e6ab3952ffa32410f9f3db11903fcb0c6d6f41f
Alejandro Ferrera <aleferrera@hotmail.com>
2016-02-27 00:07:51 -0600

    Validaciones en registro implementadas

--- js/controllers/RegisterController.js
+++ js/controllers/RegisterController.js
@@ -1,13 +1,33 @@
 var app = angular.module('VinculacionApp');
 
 app.controller('RegistroCtrl', ['$scope', 'registro', function($scope, registro) {
-    $scope.numCuenta;
+    $scope.correo;
     $scope.password;
+    $scope.numCuenta;
+    $scope.nombre;
+    $scope.campus;
+    $scope.idCarrera;
+    $scope.carrera;
     
     $scope.registrarAlumno = function() {
-        registro.registroAlumno({numCuenta: $scope.numCuenta, password: $scope.password}).success(function(response) {
+        registro.registroAlumno({
+            IdNumber: $scope.numCuenta,
+            Name: $scope.nombre,
+            Password: $scope.password,
+            Campus: $scope.campus,
+            Email: $scope.correo,
+            Major: {
+                MajorId : $scope.idCarrera,
+                Name: $scope.carrera
+            }
+    }).success(function(response) {
             console.log(response);
             console.log("Alumno registrado");
         })
+        console.log("Registrado");
+    }
+    
+    $scope.validarCorreo = function() {
+        return $scope.correo.indexOf("@unitec.edu", $scope.correo.length - 11) >= 0 ? true : false;
     }
 }]);
--- js/services/RegisterService.js
+++ js/services/RegisterService.js
@@ -1,7 +1,7 @@
 var app = angular.module('VinculacionApp');
 
 app.service('registro', function($http) {
-    var direccion = 'http://movieapp-sitepointdemos.rhcloud.com/api/movies';//Direccion temporal
+    var direccion = 'http://jsonplaceholder.typicode.com/posts';//Direccion temporal
     
     this.registroAlumno = function(data) {
         return $http.post(direccion, data);
