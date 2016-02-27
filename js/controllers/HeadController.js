3266933fad64882ad38d97bf94f6f7ec27b52719
wupa9 <wupaz9@gmail.com>
2016-02-27 16:17:38 -0600

    Remove styles array

--- js/controllers/HeadController.js
+++ js/controllers/HeadController.js
@@ -1,10 +1,6 @@
 angular.module('VinculacionApp')
 
 	.controller('HeadCtrl', [ '$rootScope', function($rootScope){
-		$rootScope.styles = [
-			"css/registro.css"
-		];
-
 		$rootScope.activeStyles = [
 			"css/registro.css"
 		];
