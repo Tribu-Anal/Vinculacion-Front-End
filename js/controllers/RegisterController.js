a58d0003c216268a4f55122a6a8af919d03ecefe
Alejandro Ferrera <aleferrera@hotmail.com>
2016-02-27 23:22:47 -0600

    Sustitucion de  por controlador y adicion de ngPattern

--- js/controllers/RegisterController.js
+++ js/controllers/RegisterController.js
@@ -1,33 +1,31 @@
 var app = angular.module('VinculacionApp');
 
-app.controller('RegistroCtrl', ['$scope', 'registro', function($scope, registro) {
-    $scope.correo;
-    $scope.password;
-    $scope.numCuenta;
-    $scope.nombre;
-    $scope.campus;
-    $scope.idCarrera;
-    $scope.carrera;
+app.controller('RegistroCtrl', ['registro', function(registro) {
+    var controlador = this;
     
-    $scope.registrarAlumno = function() {
+    controlador.correo;
+    controlador.password;
+    controlador.numCuenta;
+    controlador.nombre;
+    controlador.campus;
+    controlador.idCarrera;
+    controlador.carrera;
+    
+    controlador.registrarAlumno = function() {
         registro.registroAlumno(JSON.stringify({
-            IdNumber: $scope.numCuenta,
-            Name: $scope.nombre,
-            Password: $scope.password,
-            Campus: $scope.campus,
-            Email: $scope.correo,
+            IdNumber: controlador.numCuenta,
+            Name: controlador.nombre,
+            Password: controlador.password,
+            Campus: controlador.campus,
+            Email: controlador.correo,
             Major: {
-                MajorId : $scope.idCarrera,
-                Name: $scope.carrera
+                MajorId : controlador.idCarrera,
+                Name: controlador.carrera
             }
     })).success(function(response) {
             console.log(response);
             console.log("Alumno registrado");
         })
         console.log("Registrado");
-    }
-    
-    $scope.validarCorreo = function() {
-        return $scope.correo.indexOf("@unitec.edu", $scope.correo.length - 11) >= 0 ? true : false;
     }
 }]);
--- js/services/RegisterService.js
+++ js/services/RegisterService.js
@@ -1,7 +1,7 @@
 var app = angular.module('VinculacionApp');
 
 app.service('registro', function($http) {
-    var direccion = 'http://proyecto-vinculacion.getsandbox.com/users';//Direccion temporal
+    var direccion = 'http://proyecto-vinculacion.getsandbox.com/users';//http://vinculacionbackend.apphb.com/api/Students
     
     this.registroAlumno = function(data) {
         return $http.post(direccion, data);
