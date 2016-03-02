394b02b9ed468e1084adafdb3af84c92d9c80323
wupa9 <wupaz9@gmail.com>
2016-03-02 01:48:42 -0600

    Implement basic navigation

--- js/controllers/NavigationController.js
+++ js/controllers/NavigationController.js
@@ -1,5 +1,21 @@
 angular.module('VinculacionApp')
 
 	.controller('NavCtrl', [ function() {
-		
+		this.navItems = [ 
+						  { title: "Home", ref: "home", active: true },
+						  { title: "Proyectos", ref: "home.proyectos", active: false },
+						  { title: "Solicitudes", ref: "home.solicitudes", active: false },
+						  { title: "Log Out", ref: "landing", active: false }
+						];
+		var activeItem = this.navItems[0];
+
+		this.navItemClicked = function(itemIndex) {
+			activeItem.active = false;
+			this.setActiveItem(itemIndex);
+		};
+
+		this.setActiveItem = function(itemIndex) {
+			activeItem = this.navItems[itemIndex];
+			activeItem.active = true;
+		};
 	}]);
