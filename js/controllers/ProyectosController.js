cd560fc47423224410c790502495e738383a79e8
Alejandro Ferrera <aleferrera@hotmail.com>
2016-03-06 13:18:49 -0600

    Project's controller updated to receive one or many projects

--- js/controllers/ProyectosController.js
+++ js/controllers/ProyectosController.js
@@ -1,13 +1,16 @@
 var app = angular.module('VinculacionApp');
 
-app.controller('ProyectosCtrl', ['proyectos', function(proyectos) {
+app.controller('ProyectosCtrl', ['proyectos', 'toaster', function(proyectos, toaster) {
     var controlador = this;
     
     controlador.proyectos = [];
+    controlador.idProyecto = "";
     
-    proyectos.getProyectos(controlador.numCuenta, function(response) {        
+    proyectos.getProyectos(controlador.idProyecto, function(response) {
         console.log(response.data);
         controlador.proyectos.push(response.data);
+    }, function(response) {
+        toaster.pop({type: 'error', title: 'Error', body: 'No se ha podido obtener los proyectos deseados.'});
     });
     
 //    proyectos.getProyectos(controlador.numCuenta, function(response) {        
