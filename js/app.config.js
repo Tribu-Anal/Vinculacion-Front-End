00c7bdb46f70a2efb2b0ab2b287fd8548dafa8df
wupa9 <wupaz9@gmail.com>
2016-03-14 23:10:17 -0600

    Register events for state changes, loading screen, and remove resource loader

--- js/app.config.js
+++ js/app.config.js
@@ -19,78 +19,35 @@
 			.state('landing', {
 				url: '/',
 				templateUrl: '../templates/landing.html',
-				onEnter: ['ResourceLoader', function(rl) {
-					rl.swapResources([
-						"css/login.css"
-					]);
-				}],
-	            
-	        controller: 'LoginController as vm'
+	        	controller: 'LoginController as vm'
 			})
 
 			.state('dashboard', {
 				templateUrl: '../templates/dashboard.html',
-				onEnter: ['ResourceLoader', function(rl) {
-					rl.swapResources([
-						"https://fonts.googleapis.com/css?family=Ubuntu:400,500",
-						"https://fonts.googleapis.com/css?family=Roboto:400,500",
-						"css/navigation.css"
-					]);
-				}],
 				controller: 'NavigationController as nav'
 			})
 
 			.state('dashboard.home', {
 				url: '/home',
 				templateUrl: '../templates/home.html',
-				onEnter: ['ResourceLoader', function(rl) {
-					rl.swapResources([
-						"https://fonts.googleapis.com/css?family=Ubuntu:400,500",
-						"https://fonts.googleapis.com/css?family=Roboto:400,500",
-						"css/navigation.css",
-						"css/home.css"
-					]);
-				}],
 				controller: 'HomeController as vm'
 			})
 
 			.state('dashboard.projects', {
 				url: '/proyectos',
 				templateUrl: '../templates/projects.html',
-				onEnter: ['ResourceLoader', function(rl) {
-					rl.swapResources([
-						"https://fonts.googleapis.com/css?family=Ubuntu:400,500",
-						"https://fonts.googleapis.com/css?family=Roboto:400,500",
-						"css/navigation.css",
-						"css/projects.css"
-					]);
-				}],
 				controller: 'ProjectsController as vm'
 			})
 
 			.state('dashboard.project', {
 				url: '/proyectos/{projectId}',
 				templateUrl: '../templates/project.html',
-	            onEnter: ['ResourceLoader', function(rl) {
-					rl.swapResources([
-						"https://fonts.googleapis.com/css?family=Roboto:400,500",
-						"css/navigation.css",
-						"css/project.css"
-					]);
-				}],
 	            controller: 'ProjectController as vm'
 			})
 
 			.state('dashboard.requests', {
 				url: '/solicitudes',
 				templateUrl: '../templates/requests.html',
-				onEnter: ['ResourceLoader', function(rl) {
-					rl.swapResources([
-						"https://fonts.googleapis.com/css?family=Roboto",
-						"css/navigation.css",
-						"css/requests.css"
-					]);
-				}],
 				controller: "RequestsController as vm"
 			});
 	}
