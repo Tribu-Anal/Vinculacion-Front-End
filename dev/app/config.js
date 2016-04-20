f09888557835db142fe245bf12b09c835e439ffa
wupa9 <wupaz9@gmail.com>
2016-04-20 12:58:49 -0600

    Change css extensions to scss and rename app config and app run

--- dev/app/config.js
+++ dev/app/config.js
@@ -12,42 +12,44 @@
 	}
 
 	function setupRoutes($stateProvider, $urlRouterProvider) {
+		let templateDir = "../templates/components/";
+
 		$urlRouterProvider.otherwise('/');
 
 		$stateProvider
 
 			.state('landing', {
 				url: '/',
-				templateUrl: '../templates/landing.html',
+				templateUrl: templateDir + 'login/landing.html',
 	        	controller: 'LoginController as vm'
 			})
 
 			.state('dashboard', {
-				templateUrl: '../templates/dashboard.html',
+				templateUrl: templateDir + 'dashboard/dashboard.html',
 				controller: 'NavigationController as nav'
 			})
 
 			.state('dashboard.home', {
 				url: '/home',
-				templateUrl: '../templates/home.html',
+				templateUrl: templateDir + 'home/home.html',
 				controller: 'HomeController as vm'
 			})
 
 			.state('dashboard.projects', {
 				url: '/proyectos',
-				templateUrl: '../templates/projects.html',
+				templateUrl: templateDir + 'projects/projects.html',
 				controller: 'ProjectsController as vm'
 			})
 
 			.state('dashboard.project', {
 				url: '/proyectos/{projectId}',
-				templateUrl: '../templates/project.html',
+				templateUrl: templateDir + 'project/project.html',
 	            controller: 'ProjectController as vm'
 			})
 
 			.state('dashboard.requests', {
 				url: '/solicitudes',
-				templateUrl: '../templates/requests.html',
+				templateUrl: templateDir + 'requests/requests.html',
 				controller: "RequestsController as vm"
 			});
 	}
