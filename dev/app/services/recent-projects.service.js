29020b1fd82ded95fc66f3ed1d64992b2864cfd2
wupa9 <wupaz9@gmail.com>
2016-05-17 01:00:41 -0600

    Build service for caching and retreiving cached recent project dat

---
+++ dev/app/services/recent-projects.service.js
@@ -0,0 +1,56 @@
+(function() {
+	"use strict";
+
+	angular
+		.module('VinculacionApp')
+		.factory('recentProjects', recentProjects);
+    
+    recentProjects.$inject = ['TbCache'];
+
+	function recentProjects (TbCache) {  
+        var MAX_RECENT_PROJECTS = 5;
+        var recentProjects = [];
+
+        var service = {
+            cache: cache,
+            get: get
+        };
+        
+        return service;
+
+        function cache (projectId) {
+            recentProjects = get();
+
+            if (noRecentProjects()) 
+                recentProjects = [];
+
+            if (!projectAlreadyInList(projectId)) {
+                checkForOverflow();
+                cacheRecentProject(projectId);
+            }
+        }
+
+        function noRecentProjects () {
+            return recentProjects === undefined;
+        }
+
+        function projectAlreadyInList (projectId) {
+            return recentProjects.indexOf(projectId) >= 0;
+        }
+
+        function checkForOverflow () {
+            if (recentProjects.length === MAX_RECENT_PROJECTS)
+                recentProjects.pop();
+        }
+
+        function cacheRecentProject (projectId) {
+            recentProjects.unshift(projectId);
+            TbCache.put("RecentProjects", recentProjects);
+        }
+
+        function get () {
+            return TbCache.get("RecentProjects");
+        }
+
+	}
+})();
