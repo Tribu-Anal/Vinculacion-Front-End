41f4bdacaf3e72cca4519292d639a3b60c9f58d5
wupa9 <wupaz9@gmail.com>
2016-05-18 06:18:18 -0600

    Admin restrictions on main

--- dev/app/components/main/main.controller.js
+++ dev/app/components/main/main.controller.js
@@ -16,25 +16,30 @@
 		  { title: "HOME", ref: "dashboard.home", 
 		  	icon: "glyphicon glyphicon-home", 
 		  	active: $state.current.url === '/home',
+		  	show: true,
 		  	clicked: TbUtils.preventGeneralLoading },
 
 		  { title: "PROYECTOS", ref: "dashboard.projects", 
 		  	icon: "glyphicon glyphicon-th-large", 
 		  	active: $state.current.url.includes('/proyectos'),
+		  	show: true,
 		  	clicked:TbUtils.preventGeneralLoading },
 
 		  { title: "SOLICITUDES", ref: "dashboard.requests", 
 		  	icon: "glyphicon glyphicon-th-list", 
 		  	active: $state.current.url === '/solicitudes',
+		  	show: $rootScope.Role === 'ADMIN',
 		  	clicked: TbUtils.preventGeneralLoading },
 
 		  { title: "SECCIONES", ref: "dashboard.newsection", 
 		  	icon: "glyphicon glyphicon-plus", 
 		  	active: $state.current.url === '/nueva-seccion',
+		  	show: $rootScope.Role === 'ADMIN',
 		  	clicked: TbUtils.preventGeneralLoading },
 
 		  { title: "LOG OUT", ref: "landing", 
 		  	icon: "glyphicon glyphicon-log-out", active: false,
+		  	show: true,
 		  	clicked: closeSession }
 		];
 
@@ -48,6 +53,7 @@
 			vm.navItems[0].active = toState.url === '/home';
 			vm.navItems[1].active = toState.url.includes('/proyectos');
 			vm.navItems[2].active = toState.url === '/solicitudes';
+			vm.navItems[3].active = toState.url === '/nueva-seccion';
 		}
 
 	}
