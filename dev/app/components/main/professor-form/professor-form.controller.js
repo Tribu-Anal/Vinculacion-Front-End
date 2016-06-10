cf3d7c085fdcfe21d91eec1cd509e8b39058fcd1
Alejandro Ferrera <aleferrera@hotmail.com>
2016-06-10 10:43:40 -0600

    Change accountId field type to number

--- dev/app/components/main/professor-form/professor-form.controller.js
+++ dev/app/components/main/professor-form/professor-form.controller.js
@@ -5,9 +5,9 @@
         .module('VinculacionApp')
         .controller('ProfessorFormController', ProfessorFormController);
 
-    ProfessorFormController.$inject = ['$rootScope', '$state', 'professors'];
+    ProfessorFormController.$inject = ['$rootScope', '$state', '$scope', 'professors', 'TbUtils'];
 
-    function ProfessorFormController($rootScope, $state, professors) {
+    function ProfessorFormController($rootScope, $state, $scope, professors, TBUtils) {
         if ($rootScope.Role !== 'Admin') $state.go('dashboard.home');
 
         var vm = this;
@@ -22,15 +22,17 @@
         vm.registerProfessor = registerProfessor;
         
         function registerProfessor() {
+            vm.professor.accountId = $scope.accountId.toString();
             professors.registerProfessor(vm.professor, registerProfessorSuccess, registerProfessorFail);
         }
         
         function registerProfessorSuccess(response) {
-            
+            $state.go('dashboard.home');
         }
         
         function registerProfessorFail(response) {
-            console.log(response);            
+            console.log(response);
+            TbUtils.showErrorMessage('error', response, 'No se ha podido registrar al profesor', 'Error!');
         }
     }
 })();
