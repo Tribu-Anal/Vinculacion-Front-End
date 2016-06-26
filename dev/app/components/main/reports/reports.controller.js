0c6c7f1696d79d618e58e6c071583f06b7ec4d49
wupa9 <wupaz9@gmail.com>
2016-06-26 05:00:34 -0600

    UI for selecting which report to generate

--- dev/app/components/main/reports/reports.controller.js
+++ dev/app/components/main/reports/reports.controller.js
@@ -9,6 +9,22 @@
 
 	function ReportsController ($rootScope, TbUtils) {
 		var vm = this;
+
+		vm.reports = [
+			{ title: 'Reporte por Facultad', checked: false },
+			{ title: 'Reporte por Periodo', checked: false },
+			{ title: 'Reporte de Costos', checked: false },
+			{ title: 'Reporte de Beneficiarios', checked: false },
+			{ title: 'Reporte de Proyectos por Clase', checked: false },
+			{ title: 'Reporte de Proyecto Especial', checked: false },
+			{ title: 'Reporte por Carreras', checked: false },
+			{ title: 'Reporte de Horas', checked: false },
+			{ title: 'Reporte Numero de Alumnos', checked: false }
+		];
+		vm.generateReports = generateReports;
+
+		function generateReports () {
+		}
 	}
 
 })();
