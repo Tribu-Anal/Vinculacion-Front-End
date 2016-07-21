1c2fe1e2342981a17de0cc0b19de689d096bf71e
wupa9 <wupaz9@gmail.com>
2016-07-21 03:00:02 -0600

    Modularize project evaluation form

---
+++ dev/app/components/main/project-evaluation-form/project-evaluation-form.config.js
@@ -0,0 +1,26 @@
+"use strict";
+
+config.$inject = [ '$stateProvider' ];
+
+function config ($stateProvider) {
+
+	$stateProvider
+		.state('dashboard.evaluateproject', {
+			url: '/evaluacion-proyecto/{projectId}',
+			params: {
+                projectId: null
+            },
+			templateUrl: 'templates/components/main/project-evaluation-form/project-evaluation-form.html',
+			controller: "ProjectEvaluationFormController as vm"
+			onEnter: onStateEnter
+		});
+}
+
+let onStateEnter = [ '$rootScope', 
+	function (rootScope) { 
+		rootScope.viewTitle  = "Vinculacion | Evaluacion de Proyecto";
+    	rootScope.viewStyles = "main project-form";
+    }
+];
+
+module.exports = config;
---
+++ dev/app/components/main/project-evaluation-form/project-evaluation-form.controller.js
@@ -0,0 +1,24 @@
+ProjectEvaluationFormController.$inject = ['$rootScope', '$state', '$stateParams', 'projects', 'TbUtils'];
+
+function ProjectEvaluationFormController($rootScope, $state, $stateParams, projects, TBUtils) {
+    if ($rootScope.Role !== 'Professor'  || !$stateParams.projectId) $state.go('dashboard.projects');
+
+    var vm = this;
+    vm.formData = {
+        fieldHours: 0,
+        calification: 0,
+        beneficiariesQuantities: 0,
+        beneficiariGroups: ''
+    };
+    
+    vm.accountId;
+    
+    function downloadReport() {
+        console.log("onoid");
+        document.getElementById('my_iframe').src = projects.getProjectReportUrl($state.params.projectId, vm.formData.fieldHours, vm.formData.calification, vm.formData.beneficiariesQuantities, vm.formData.beneficiariGroups);
+    }
+    
+    vm.downloadReport = downloadReport;
+}
+
+module.exports = { name: 'ProjectEvaluationFormController', ctrl: ProjectEvaluationFormController };
