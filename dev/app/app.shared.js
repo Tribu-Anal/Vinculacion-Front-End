3e083a269deceafd6ccbd9d590b34e0243d95425
wupa9 <wupaz9@gmail.com>
2016-07-21 04:24:20 -0600

    Add missing shared modules

--- dev/app/app.shared.js
+++ dev/app/app.shared.js
@@ -1,13 +1,15 @@
-const tbHeaderbar = require('./shared/TB-Headerbar/tb-headerbar'),
-	  tbLoading   = require('./shared/TB-Loading/tb-loading'),
-	  tbRoundBtn  = require('./shared/TB-RoundBtn/tb-round-btn'),
-	  tbSearch    = require('./shared/TB-Search/tb-search'),
-	  tbSidebar   = require('./shared/TB-Sidebar/tb-sidebar'),
-	  tbTable     = require('./shared/TB-Table/tb-table');
+const tbButton    = require('./shared/TB-Button/tb-button.module'),
+      tbHeaderbar = require('./shared/TB-Headerbar/tb-headerbar.module'),
+	  tbLoading   = require('./shared/TB-Loading/tb-loading.module'),
+	  tbPaginator = require('./shared/TB-Paginator/tb-paginator.module'),
+	  tbRoundBtn  = require('./shared/TB-RoundBtn/tb-round-btn.module'),
+	  tbSearch    = require('./shared/TB-Search/tb-search.module'),
+	  tbSidebar   = require('./shared/TB-Sidebar/tb-sidebar.module'),
+	  tbTable     = require('./shared/TB-Table/tb-table.module');
 
 const moduleName  = 'vinculacion.shared',
-     dependencies = [ tbHeaderbar, tbLoading, tbRoundBtn, 
-	                  tbSearch, tbSidebar, tbTable ];
+     dependencies = [ tbButton, tbHeaderbar, tbLoading, tbPaginator, 
+                      tbRoundBtn, tbSearch, tbSidebar, tbTable ];
 
 angular.module(moduleName, dependencies);
 
