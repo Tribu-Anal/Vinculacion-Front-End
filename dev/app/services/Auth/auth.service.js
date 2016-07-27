a3bfb9c61e724fd6eb37f26c1b84ca53dfd1785e
Daniel Perez <danielperez@unitec.edu>
2016-07-26 19:33:41 -0600

    solo el propio estudiante ve las horas y el finiquito

--- dev/app/services/Auth/auth.service.js
+++ dev/app/services/Auth/auth.service.js
@@ -1,39 +1,45 @@
 auth.$inject = ['$http', '$cookieStore', '$rootScope'];
 
-function auth ($http, $cookieStore, $rootScope) {
-	var service = {
-		Login: Login,
-		SetCredentials: SetCredentials,
-		ClearCredentials: ClearCredentials,
-		AccountId: AccountId
-	};
+function auth($http, $cookieStore, $rootScope) {
+    var service = {
+        Login: Login,
+        SetCredentials: SetCredentials,
+        ClearCredentials: ClearCredentials,
+        AccountId: AccountId
+    };
 
-	return service;
+    return service;
 
-	function Login (username, password, successCallback, errorCallback) {
-       $http.post('http://fiasps.unitec.edu:8085/api/Login', JSON.stringify({ User: username, Password: password }))
+    function Login(username, password, successCallback, errorCallback) {       
+        $http.post('http://fiasps.unitec.edu:8085/api/Login', JSON.stringify({
+            User: username,
+            Password: password
+        }))
             .then(successCallback)
-            .catch(errorCallback);
-    };
-  
-    function SetCredentials (token) {  
-       $rootScope.globals.token = token;
-  
-       $http.defaults.headers.common['Authorization'] = token;
-       $cookieStore.put('globals', $rootScope.globals);
-    };
-  
-    function ClearCredentials () {
-        $rootScope.globals = {};
-        $cookieStore.remove('globals');
-        $http.defaults.headers.common.Authorization = 'Basic ';
-    };
+            .catch(errorCallback);    
+    };      
 
-	function AccountId(email, successCallback, errorCallback){
-		let url = 'http://fiasps.unitec.edu:8085/api/StudentByEmail/';
-		$http.post(url,JSON.stringify(email)).then(successCallback)
-			.catch(errorCallback); 
-	};
+    function SetCredentials(credentials) {         
+        $rootScope.globals.token = credentials.Token;       
+        $rootScope.globals.id = credentials.Id;         
+        $http.defaults.headers.common['Authorization'] = credentials.Token;       
+        $cookieStore.put('globals', $rootScope.globals);    
+    };      
+
+    function ClearCredentials() {        
+        $rootScope.globals = {};        
+        $cookieStore.remove('globals');        
+        $http.defaults.headers.common.Authorization = 'Basic ';    
+    };
+
+    function AccountId(email, successCallback, errorCallback) {
+        let url = 'http://fiasps.unitec.edu:8085/api/StudentByEmail/';
+        $http.post(url, JSON.stringify(email)).then(successCallback)
+            .catch(errorCallback);
+    };
 }
 
-module.exports = { name: 'auth', srvc: auth };
+module.exports = {
+    name: 'auth',
+    srvc: auth
+};
