14f55ef88b146102384e45af043be944756cc121
wupa9 <wupaz9@gmail.com>
2016-07-31 06:08:12 -0600

    Add service generator

--- plopfile.js
+++ plopfile.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const componentGen = require('./generators/component.generator');
+const componentGen = require('./generators/component.generator'),
+      serviceGen   = require('./generators/service.generator');
 
 module.exports = plop => {
 	plop.addPrompt('directory', require('inquirer-directory'));
 
     plop.setGenerator(componentGen.name, componentGen.generator);
-    // plop.setGenerator();
+    plop.setGenerator(serviceGen.name, serviceGen.generator);
     // plop.setGenerator();
     // plop.setGenerator();
 };
