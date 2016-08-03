d6bd91e698d18710c529f2d3c740acc0dec74f4a
wupa9 <wupaz9@gmail.com>
2016-08-03 00:35:00 -0600

    Add generator for runs

--- plopfile.js
+++ plopfile.js
@@ -5,7 +5,8 @@ const componentGen  = require('./generators/component.generator'),
       sharedGen     = require('./generators/shared.generator'),
       bundleGen     = require('./generators/bundle.generator'),
       controllerGen = require('./generators/controller.generator'),
-      configGen     = require('./generators/config.generator');
+      configGen     = require('./generators/config.generator'),
+      runGen        = require('./generators/run.generator');
 
 module.exports = plop => {
 	plop.addHelper('sharedCase', require('./generators/helpers').sharedCase);
@@ -17,4 +18,5 @@ module.exports = plop => {
     plop.setGenerator(bundleGen.name, bundleGen.generator);
     plop.setGenerator(controllerGen.name, controllerGen.generator);
     plop.setGenerator(configGen.name, configGen.generator);
+    plop.setGenerator(runGen.name, runGen.generator);
 };
