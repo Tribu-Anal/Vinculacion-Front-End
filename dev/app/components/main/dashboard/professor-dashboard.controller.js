d030146c46efa5965d954541d7e2510c4de915b5
Alejandro Ferrera <aleferrera@hotmail.com>
2016-08-08 15:52:43 -0600

    Show projects

--- dev/app/components/main/dashboard/professor-dashboard.controller.js
+++ dev/app/components/main/dashboard/professor-dashboard.controller.js
@@ -4,20 +4,13 @@ ProfessorDashboardController.$inject = ['$rootScope', '$stateParams', 'TbUtils',
 function ProfessorDashboardController($rootScope, $stateParams, TbUtils, sections, students, tableContent) {
     var vm = this;
 
-    vm.sections        = [];
-    // vm.sectionStudents = [];
-    // vm.studentHours    = [];
     vm.currentProjects = [];
     vm.sectionsTable = TbUtils.getTable(['Codigo', 'Clase', 'Periodo']);
+    vm.projectsTable = TbUtils.getTable(['Id Proyecto', 'Nombre', 'Evaluar Proyecto']);
     vm.preventGeneralLoading = TbUtils.preventGeneralLoading;
 
-    vm.sectionsLoading        = true;
-    //vm.studentsLoading        = true;
-    // vm.studentsHoursLoading   = true;
-    // vm.currentProjectsLoading = true;
-
-    // vm.getStudents        = getStudents;
-    // vm.getStudentsHours   = getStudentsHours;
+    vm.sectionsLoading = true;
+    //vm.projectsLoading = true;
     vm.getCurrentProjects = getCurrentProjects;
 
     sections.getCurrentSections(currentSectionsSuccess, currentSectionsFail);
@@ -41,6 +34,8 @@ function ProfessorDashboardController($rootScope, $stateParams, TbUtils, section
         }
 
         vm.sectionsLoading = false;
+        console.log(vm.sectionsTable.body);
+        getCurrentProjects();
     }
 
     function currentSectionsFail(response) {
@@ -57,44 +52,10 @@ function ProfessorDashboardController($rootScope, $stateParams, TbUtils, section
         return section.Period.Number + " - " + section.Period.Year
     }
 
-    // function getStudents(sectionId) {
-    //     sections.getStudents(sectionId, getStudentsSuccess, getStudentsFail);
-    // }
-
-    // function getStudentsSuccess(response) {
-    //     vm.sectionStudents = [];
-    //     TbUtils.fillListWithResponseData(response.data, vm.sectionStudents);
-    //     vm.studentsLoading = false;
-    //     console.log(vm.sectionStudents);
-    //     getStudentsHours();
-    // }
-
-    // function getStudentsFail(response) {
-    //     console.log(response);
-    // }
-
-    // function getStudentsHours() {
-    //     vm.studentHours = [];
-
-    //     for(student in vm.sectionStudents) {
-    //         students.getHours(vm.sectionStudents[student].AccountId, HoursSuccess, HoursFail);
-    //     }
-    // }
-
-    // function HoursSuccess(response) {
-    //     vm.studentHours.push(response.data);
-    //     console.log(vm.studentHours);
-    // }
-
-    // function HoursFail(response) {
-    //     console.log(response);
-    // }
-
     function getCurrentProjects() {
-        vm.currentProjects = [];
 
-        for(section in vm.sections) {
-            getProjectsBySection(vm.sections[section].Id);
+        for(section in vm.sectionsTable.body) {
+            getProjectsBySection(vm.sectionsTable.body[section].data.Id);
         }
     }
 
@@ -103,11 +64,31 @@ function ProfessorDashboardController($rootScope, $stateParams, TbUtils, section
     }
 
     function getProjectsSuccess(response) {
-        for(prj in response.data) {
-            if(!findDuplicatesProjects(response.data[prj].Id))
-                vm.currentProjects.push(response.data[prj]);
+        // for(prj in response.data) {
+        //     if(!findDuplicatesProjects(response.data[prj].Id))
+        //         vm.currentProjects.push(response.data[prj]);
+        // }
+        // console.log(vm.currentProjects);
+
+            for (let i = 0; i < response.data.length; i++) {
+                let project = response.data[i];
+
+                // for(prj in response.data) {
+                //     if(!findDuplicatesProjects(response.data[prj].Id)
+                //         vm.currentProjects.push(response.data[prj]);
+                // }
+
+                let newTableElement = {
+                    content: [
+                        tableContent.createALableElement(project.ProjectId),
+                        tableContent.createALableElement(project.Name)
+                    ],
+
+                    data: project
+                };
+
+                vm.projectsTable.body.push(newTableElement);        
         }
-        console.log(vm.currentProjects);
     }
 
     function getProjectsFail(response) {
@@ -115,8 +96,8 @@ function ProfessorDashboardController($rootScope, $stateParams, TbUtils, section
     }
 
     function findDuplicatesProjects(projectId) {
-        for(prj in vm.currentProjects) {
-            if(projectId === vm.currentProjects[prj].Id) return true;
+        for(prj in vm.projectsTable.body) {
+            if(projectId === vm.projectsTable.body[prj].data.Id) return true;
         }
 
         return false;
