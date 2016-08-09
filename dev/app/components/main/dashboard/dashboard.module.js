3a205b9d1db4f7b80cfb5a11806b068f7619e941
wupa9 <wupaz9@gmail.com>
2016-08-09 13:24:28 -0600

    Remove admin dashboard

--- dev/app/components/main/dashboard/dashboard.module.js
+++ dev/app/components/main/dashboard/dashboard.module.js
@@ -1,7 +1,6 @@
 const  config     = require('./dashboard.config'),
 	   StudentController = require('./student-dashboard.controller'),
-	   ProfessorController = require('./professor-dashboard.controller'),
-		 AdminController = require('./admin-dashboard.controller');
+	   ProfessorController = require('./professor-dashboard.controller');
 
 const moduleName   = 'dashboard.component',
       dependencies = [];
@@ -9,7 +8,6 @@ const moduleName   = 'dashboard.component',
 angular.module(moduleName, dependencies)
 	.config(config)
 	.controller(StudentController.name, StudentController.ctrl)
-	.controller(ProfessorController.name, ProfessorController.ctrl)
-	.controller(AdminController.name, AdminController.ctrl);
+	.controller(ProfessorController.name, ProfessorController.ctrl);
 
 module.exports = moduleName;
