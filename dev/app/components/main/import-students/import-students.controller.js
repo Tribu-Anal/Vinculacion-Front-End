04d8910a45fae83881dc34a7a0e434576ca6ac51
wupa9 <wupaz9@gmail.com>
2016-08-24 01:56:03 -0600

    Add import-students component

---
+++ dev/app/components/main/import-students/import-students.controller.js
@@ -0,0 +1,7 @@
+ImportStudentsController.$inject = [];
+
+function ImportStudentsController () {
+	const vm = this;
+}
+
+module.exports = { name: 'ImportStudentsController', ctrl: ImportStudentsController };
