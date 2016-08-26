1e622ece1927b8fc8723543558f1ab1f7d3aac6d
wupa9 <wupaz9@gmail.com>
2016-08-26 04:11:41 -0600

    Get parsed excel API call

--- dev/app/services/Students/students.service.js
+++ dev/app/services/Students/students.service.js
@@ -1,13 +1,14 @@
-students.$inject = ['$http'];
+students.$inject = ['$http', '$q'];
 
-function students ($http) {
+function students ($http, $q) {
 	const url = 'http://fiasps.unitec.edu:' + PORT + '/api/Students';
 
 	const service = {
 		get: get,
 		getHours: getHours,
 		getAccountId: getAccountId,
-		getSectionHours: getSectionHours
+		getSectionHours: getSectionHours,
+		getParsedStudentsExcel: getParsedStudentsExcel
 	};
 
 	return service;
@@ -34,6 +35,16 @@ function students ($http) {
 	function getSectionHours(accountId, successCallback, errorCallback){
 				$http.get(url + '/'+accountId+'/SectionHours').then(successCallback)
 						.catch(errorCallback);
+	}
+
+	function getParsedStudentsExcel (data) {
+		const deferred = $q.defer();
+
+      	$http.get(url+'/Parse/'+data)
+	        .success(response => { deferred.resolve(response.data); })
+	        .error(reject => { deferred.reject('No se pudo importar el archivo.'); });
+
+	    return deferred.promise;
 	}
 
 }
