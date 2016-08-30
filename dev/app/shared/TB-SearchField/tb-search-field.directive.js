07dedc210957e8d2337de948285270a6a53501a4
Daniel Perez <danielperez@unitec.edu>
2016-08-30 10:09:43 -0600

    added placeholder in search field directive

--- dev/app/shared/TB-SearchField/tb-search-field.directive.js
+++ dev/app/shared/TB-SearchField/tb-search-field.directive.js
@@ -1,11 +1,19 @@
-
 function tbSearchField() {
-	var directive = {
-		restrict: 'E',
-		templateUrl: 'templates/shared/TB-SearchField/tb-search-field.html'
-	}
+    var directive = {
+        restrict: 'E',
+        templateUrl: 'templates/shared/TB-SearchField/tb-search-field.html',
+        scope: {
+            placeholder: '@?'
+        },
+        link: function(scope, element, attrs) {
+            scope.placeholder = scope.placeholder ? scope.placeholder : 'Buscar';
+        }
+    }
 
-	return directive;
+    return directive;
 }
 
-module.exports = { name: 'tbSearchField', drtv: tbSearchField };
+module.exports = {
+    name: 'tbSearchField',
+    drtv: tbSearchField
+};
