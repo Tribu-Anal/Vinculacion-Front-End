a0a023daf55772f6cdc0a50a72375cf315d97b76
wupa9 <wupaz9@gmail.com>
2016-08-30 17:49:30 -0600

    Remove comments

--- dev/app/shared/TB-Table/tb-table.controller.js
+++ dev/app/shared/TB-Table/tb-table.controller.js
@@ -7,31 +7,6 @@ function TbTableController($scope, $state, TbUtils, tableBuilder) {
     $scope.headers = vm.table.headers;
     $scope.rows = vm.table.rows;
     
-    // vm.onRowClick = onRowClick;
-    // vm.onBtnClick = onBtnClick;
-
-    // let btnClicked = false;
-
-    // function onRowClick(row) {
-    //     if (!btnClicked && $scope.ref) {
-    //         TbUtils.preventGeneralLoading();
-    //         $state.go($scope.ref, {
-    //             sectionId: row.data.sectionId ? row.data.sectionId : row.data.Id,
-    //             projectId: row.data.projectId ? row.data.projectId : row.data.Id
-    //         });
-    //     }
-
-    //     btnClicked = false;
-    // }
-
-    // function onBtnClick(row) {
-    //     btnClicked = true;
-    //     vm.loseFocus();
-    // }
-
-    // vm.loseFocus = function() {
-    //     $(':focus').blur();
-    // };
 }
 
 module.exports = {
