45342500d68eef8d378ef4ac458a1a14bf77a7b6
Kelvin Nose <kelvinjosse@gmail.com>
2016-08-30 13:13:56 -0600

    Implement admin dashboard controller

---
+++ dev/app/components/main/dashboard/admin-dashboard.controller.js
@@ -0,0 +1,7 @@
+AdminDashboardController.$inject = [];
+
+function AdminDashboardController () {
+	const vm = this;
+}
+
+module.exports = { name: 'AdminDashboardController', ctrl: AdminDashboardController };
