b8e447746418ab5b38b76efb25cc25d2f1218db2
wupa9 <wupaz9@gmail.com>
2016-08-31 01:57:45 -0600

    Remove unnecessary functions and add emptyTable

--- dev/app/services/TbTable/TableBuilder/table-builder.service.js
+++ dev/app/services/TbTable/TableBuilder/table-builder.service.js
@@ -3,35 +3,28 @@ tableBuilder.$inject = [ 'tableContent' ];
 function tableBuilder (tableContent) {
 	const service = {
 		newTable: newTable,
-        append: append,
-        indexOf: indexOf,
-        remove: remove
+        emptyTable: emptyTable
 	};
 
-	function newTable (model) {
+	function newTable (schema, model) {
         const table = {
-            headers: model.headers,
+            headers: schema.headers,
             rows: []
         };
 
-        const data = model.data;
-        const schema = model.schema;
-
-        for (const obj of data)
-            append(table, schema, obj);
+        for (const obj of model)
+            append(table, schema.rows, obj);
 
         return table;
     }
 
-    function append (table, schema, data) {
-        if (!table || !schema || !data) return;
-
+    function append (table, rowSchema, data) {
         const row = {
             elements: [],
             data: data
         };
 
-        for (const sc of schema) {
+        for (const sc of rowSchema) {
             let elem = tableContent.createNewElement(sc.type, sc.props, data);
             row.elements.push(elem);
         }
@@ -39,19 +32,11 @@ function tableBuilder (tableContent) {
         table.rows.push(row);
     }
 
-    function indexOf (table, data) {
-        if (!table || !table.rows || !data) return -1;
-        return table.rows.indexOf(data);
-    }
-
-    function remove (table, index) {
-        if (!table || !index) return;
-
-        if (typeof index === 'object')
-            index = indexOf(table, index);
-
-        if (index >= 0)
-            table.rows.splice(index, 1);
+    function emptyTable () {
+        return {
+            headers: [],
+            rows: []
+        };
     }
 
 	return service;
