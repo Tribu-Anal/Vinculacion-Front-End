048fcb539f0ef1624f4540d406b0d48550a0bb87
wupa9 <wupaz9@gmail.com>
2016-08-31 03:59:41 -0600

    Update edit hours to new tbTable

---
+++ dev/app/table-schemas/edit-hours-table-schema.js
@@ -0,0 +1,9 @@
+module.exports = {
+	headers: ['Número de Cuenta', 'Nombre', 'Horas en este proyecto'],
+	rows: [
+		{ type: 'label', props: { text: obj => obj.User.AccountId }  },
+		{ type: 'label', props: { text: obj => obj.User.Name }  },
+		{ type: 'input', props: { model: obj => obj.User.Hours ? obj.User.Hours.Amount : 0, disable: '', 
+			                      inputDisabled: false, type: 'number', min: 0, max: 100 } }
+	]
+};
