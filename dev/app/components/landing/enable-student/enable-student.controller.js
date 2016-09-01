fb97c7546812e9dcab02fb9000aa5a448b10fd1f
Kelvin Nose <kelvinjosse@gmail.com>
2016-08-31 19:05:09 -0600

    Add enable student on controller

--- dev/app/components/landing/enable-student/enable-student.controller.js
+++ dev/app/components/landing/enable-student/enable-student.controller.js
@@ -1,8 +1,38 @@
-EnableStudentController.$inject = ['$rootScope', '$location', 'auth',
-                                'role', 'toaster', 'TbUtils', '$state'];
+EnableStudentController.$inject = ['$rootScope', 'toaster', 'TbUtils', '$state', 'students'];
 
-function EnableStudentController ($rootScope, $location, auth, role, toaster, TbUtils, $state) {
+function EnableStudentController ($rootScope, toaster, TbUtils, $state, students) {
 	const vm = this;
+
+  vm.email = "";
+  vm.accountId = "";
+  vm.password = "";
+  vm.confirm = "";
+
+  vm.enableStudent = EnableStudent;
+
+  function EnableStudent(){
+    let student = {
+      AccountId: vm.AccountId,
+      Email: vm.email,
+      Password: vm.password
+    }
+    console.log("Data that is going to be sent: "+JSON.stringify(student));
+    students.enableStudent(vm.student, enableStudentSuccess, enableStudentFail);
+  }
+
+  function enableStudentSuccess(response){
+    console.log(response);
+    TbUtils.displayNotification('success', 'Alumno habilitado exitosamente!', 'Habilitado');
+    $state.go('landing.login');
+  }
+
+  function enableStudentFail(response){
+    console.log(response);
+    TbUtils.showErrorMessage('error',
+                             'No se encontro una cuenta valida con los datos ingresados',
+                             'Error de Habilitacion');
+  }
+
 }
 
 module.exports = { name: 'EnableStudentController', ctrl: EnableStudentController };
