5cfc5e73650200798d6759861fd3a2e30e2bc3fd
Kelvin Nose <kelvinjosse@gmail.com>
2016-08-31 20:00:16 -0600

    Change on controller for sending student data

--- dev/app/components/landing/enable-student/enable-student.controller.js
+++ dev/app/components/landing/enable-student/enable-student.controller.js
@@ -1,35 +1,35 @@
-EnableStudentController.$inject = ['$rootScope', 'toaster', 'TbUtils', '$state', 'students'];
+EnableStudentController.$inject = ['$rootScope', 'TbUtils', 'students'];
 
-function EnableStudentController ($rootScope, toaster, TbUtils, $state, students) {
+function EnableStudentController ($rootScope, TbUtils,  students) {
 	const vm = this;
 
   vm.email = "";
   vm.accountId = "";
   vm.password = "";
   vm.submitting = false;
-  vm.enableStudent = EnableStudent;
+  vm.enableStudent = enableStudent;
 
-  function EnableStudent(){
+  function enableStudent(){
     vm.submitting = true;
     let student = {
       AccountId: vm.accountId.toString(),
       Email: vm.email,
       Password: vm.password
-    }
-    students.enableStudent(vm.student, enableStudentSuccess, enableStudentFail);
+    };
+    students.enableStudent(student, enableStudentSuccess, enableStudentFail);
   }
 
   function enableStudentSuccess(response){
-    vm.submitting = false;
     TbUtils.displayNotification('success', 'Alumno habilitado exitosamente!', 'Habilitado');
-    $state.go('landing.login');
+    vm.submitting = false;
+    TbUtils.go('landing.login');
   }
 
   function enableStudentFail(response){
-    vm.submitting = false;
-    TbUtils.showErrorMessage('error',
+    TbUtils.showErrorMessage('error', response,
                              'No se encontro una cuenta valida con los datos ingresados',
                              'Error de Habilitacion');
+    vm.submitting = false;
   }
 
 }
