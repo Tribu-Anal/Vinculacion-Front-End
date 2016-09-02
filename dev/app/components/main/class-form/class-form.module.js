06146c31a34a42d49ddc9f02a701f6198ac0f87b
wupa9 <wupaz9@gmail.com>
2016-09-02 05:07:38 -0600

    Rename class-form controller to new-class and add edit-class

--- dev/app/components/main/class-form/class-form.module.js
+++ dev/app/components/main/class-form/class-form.module.js
@@ -1,11 +1,13 @@
-const  config     = require('./class-form.config'),
-	   controller = require('./class-form.controller');
+const  config              = require('./class-form.config'),
+	   newClassController  = require('./new-class.controller'),
+	   editClassController = require('./edit-class.controller');
 
 const moduleName   = 'class-form.component',
       dependencies = [];
 
 angular.module(moduleName, dependencies)
 	.config(config)
-	.controller(controller.name, controller.ctrl);
+	.controller(newClassController.name, newClassController.ctrl)
+	.controller(editClassController.name, editClassController.ctrl);
 
 module.exports = moduleName;
