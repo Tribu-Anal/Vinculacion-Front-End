b7f7b58e3faa80e5e57b2add3154f305df56f097
wupa9 <wupaz9@gmail.com>
2016-09-02 15:06:00 -0600

    Remove and disconnect TB-Search

--- dev/app/app.shared.js
+++ dev/app/app.shared.js
@@ -1,17 +1,16 @@
-const tbButton    = require('./shared/TB-Button/tb-button.module'),
-      tbHeaderbar = require('./shared/TB-Headerbar/tb-headerbar.module'),
-	  tbLoading   = require('./shared/TB-Loading/tb-loading.module'),
-	  tbPaginator = require('./shared/TB-Paginator/tb-paginator.module'),
+const tbButton      = require('./shared/TB-Button/tb-button.module'),
+      tbHeaderbar   = require('./shared/TB-Headerbar/tb-headerbar.module'),
+	  tbLoading     = require('./shared/TB-Loading/tb-loading.module'),
+	  tbPaginator   = require('./shared/TB-Paginator/tb-paginator.module'),
 	  tbSearchField = require('./shared/TB-SearchField/tb-search-field.module'),
-	  tbRoundBtn  = require('./shared/TB-RoundBtn/tb-round-btn.module'),
-	  tbSearch    = require('./shared/TB-Search/tb-search.module'),
-	  tbSidebar   = require('./shared/TB-Sidebar/tb-sidebar.module'),
-	  tbTable     = require('./shared/TB-Table/tb-table.module'),
-	  tbUploadBtn = require('./shared/TB-Upload-Btn/tb-upload-btn.module');
+	  tbRoundBtn    = require('./shared/TB-RoundBtn/tb-round-btn.module'),
+	  tbSidebar     = require('./shared/TB-Sidebar/tb-sidebar.module'),
+	  tbTable       = require('./shared/TB-Table/tb-table.module'),
+	  tbUploadBtn   = require('./shared/TB-Upload-Btn/tb-upload-btn.module');
 
 const moduleName  = 'vinculacion.shared',
-     dependencies = [ tbButton, tbHeaderbar, tbLoading, tbPaginator, 
-                      tbSearchField, tbRoundBtn, tbSearch, tbSidebar, tbTable, tbUploadBtn ];
+     dependencies = [ tbButton, tbHeaderbar, tbLoading, tbPaginator, tbSearchField, 
+                      tbRoundBtn, tbSidebar, tbTable, tbUploadBtn ];
 
 angular.module(moduleName, dependencies);
 
