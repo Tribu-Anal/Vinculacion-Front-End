aeecdce865046509acaec8ac7bd771875d8d4dae
wupa9 <wupaz9@gmail.com>
2016-09-02 16:54:40 -0600

    Upgrade tbSearchField

--- dev/app/shared/TB-SearchField/tb-search-field.directive.js
+++ dev/app/shared/TB-SearchField/tb-search-field.directive.js
@@ -1,9 +1,35 @@
-function tbSearchField() {
+tbSearchField.$inject = [ 'filterFilter' ];
+
+function tbSearchField(filterFilter) {
     var directive = {
         restrict: 'E',
+        scope: {
+            obj: '=',
+            results: '=',
+            data: '=',
+            auto: '=?',
+            min: '=?',
+            placeholder: '@?'
+        },
         templateUrl: 'templates/shared/TB-SearchField/tb-search-field.html',
-        link: function(scope, element, attrs) {
-            scope.placeholder = scope.placeholder ? scope.placeholder : 'Buscar';
+        link: scope => {
+            if (!scope.placeholder) scope.placeholder = "Ingrese su busqueda";
+            if (scope.auto && !scope.min) scope.min = 1;
+
+            scope.search = term => {
+                if (typeof scope.obj === 'function')
+                    scope.results = filterFilter(scope.data, scope.obj(term));
+                else
+                    scope.results = scope.data;
+            };
+
+            scope.$watch('searchText', term => {
+                if (scope.auto && term.length >= scope.min)
+                    scope.search(term);
+                else
+                    scope.results = scope.data;
+            });
+
         }
     }
 
