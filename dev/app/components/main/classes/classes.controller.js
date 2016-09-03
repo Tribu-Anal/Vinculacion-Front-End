b1b9bf5bb2930520563e82baa99ba445300ae158
wupa9 <wupaz9@gmail.com>
2016-09-02 23:57:30 -0600

    Reorder code

--- dev/app/components/main/classes/classes.controller.js
+++ dev/app/components/main/classes/classes.controller.js
@@ -3,18 +3,18 @@ ClassesController.$inject = [ 'TbUtils', 'classes' ];
 function ClassesController (TbUtils, classes) {
 	const vm = this;
 
+    vm.searchResults = [];
+    vm.classObj = term => { return { Name: term }; };
+
+    vm.classes = [];
+    vm.tableSchema = require('../../../table-schemas/classes-table-schema');
+
     vm.pageSize = 10;
     vm.get = classes.getAndLoad;
     vm.hideLoadBtn = () => vm.classes.length !== vm.searchResults.length;
 
-	vm.classes = [];
-	vm.tableSchema = require('../../../table-schemas/classes-table-schema');
-
-    vm.searchResults = [];
-    vm.classObj = term => { return { Name: term }; };
-
+    vm.goToNewClass = () => { TbUtils.go('main.new-class'); };
     vm.editClass = _class => { TbUtils.go('main.edit-class', { _class: btoa(JSON.stringify(_class)) }); };
-	vm.goToNewClass = () => { TbUtils.go('main.new-class'); };
 
 	vm.classesLoading = true;
 
