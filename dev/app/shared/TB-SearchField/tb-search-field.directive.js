479c1c2aaf60254f519b8472903d7d58f0d2204d
wupa9 <wupaz9@gmail.com>
2016-09-03 17:57:01 -0600

    Add searchAll feature to search field

--- dev/app/shared/TB-SearchField/tb-search-field.directive.js
+++ dev/app/shared/TB-SearchField/tb-search-field.directive.js
@@ -7,23 +7,37 @@ function tbSearchField(filterFilter) {
             obj: '=?',
             results: '=?',
             data: '=?',
+            getAll: '=?',
+            loading: '=?',
             auto: '=?',
             min: '=?',
-            placeholder: '@?',
-            search: '=?'
+            placeholder: '@?'
         },
         templateUrl: 'templates/shared/TB-SearchField/tb-search-field.html',
         link: scope => {
             if (!scope.placeholder) scope.placeholder = "Ingrese su busqueda";
             if (scope.auto && !scope.min) scope.min = 1;
 
-            if (!scope.search)
-                scope.search = term => {
-                    if (typeof scope.obj === 'function')
-                        scope.results = filterFilter(scope.data, scope.obj(term));
-                    else
-                        scope.results = scope.data;
-                };
+            scope.all = null;
+
+            scope.search = term => {
+                if (typeof scope.obj === 'function')
+                    scope.results = filterFilter(scope.data, scope.obj(term));
+                else
+                    scope.results = scope.data;
+            };
+
+            scope.searchAll = term => {
+                if (typeof scope.getAll === 'function' && !scope.all) {
+                    scope.loading = true;
+                    scope.getAll(resp => { scope.all = resp.data; 
+                                           scope.results = filterFilter(scope.all, scope.obj(term)); }, 
+                                 resp => { TbUtils.showErrorMessage(resp.data) },
+                                 ()   => { scope.loading = false; });
+                }
+                else
+                    scope.results = filterFilter(scope.all, scope.obj(term));
+            };
 
             scope.$watch('searchText', term => {
                 if (scope.auto && term.length >= scope.min)
