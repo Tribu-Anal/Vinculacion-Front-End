55fa7acd8d8a5baf4a4babe94e80d97166283a23
wupa9 <wupaz9@gmail.com>
2016-09-02 22:57:15 -0600

    Mod with tbLoadMoreBtn and getAndLoad

--- dev/app/components/main/classes/classes.controller.js
+++ dev/app/components/main/classes/classes.controller.js
@@ -1,46 +1,24 @@
 ClassesController.$inject = [ 'TbUtils', 'classes' ];
 
 function ClassesController (TbUtils, classes) {
-	const vm = this,
-	      pageSize = 10;
+	const vm = this;
 
-    let page = 0;
+    vm.pageSize = 10;
+    vm.get = classes.getAndLoad;
+    vm.hideLoadBtn = () => vm.classes.length !== vm.searchResults.length;
 
 	vm.classes = [];
 	vm.tableSchema = require('../../../table-schemas/classes-table-schema');
 
-    vm.classObj = term => { return { Name: term }; };
     vm.searchResults = [];
+    vm.classObj = term => { return { Name: term }; };
 
-	vm.loadMore = loadMore;
     vm.editClass = _class => { TbUtils.go('main.edit-class', { _class: btoa(JSON.stringify(_class)) }); };
-	vm.preventGeneralLoading = TbUtils.preventGeneralLoading;
+	vm.goToNewClass = () => { TbUtils.go('main.new-class'); };
+
 	vm.classesLoading = true;
-	vm.loadingMore = false;
 
-	loadMore();
-
-	function loadMore() {
-        if (vm.loadingMore) return;
-
-        vm.loadingMore = true;
-        classes.get(page, pageSize, loadMoreSuccess, loadMoreFailure);
-    }
-
-    function loadMoreSuccess (response) {
-    	page++;
-
-    	TbUtils.fillListWithResponseData(response.data, vm.classes);
-
-    	vm.loadingMore = false;
-    	vm.classesLoading = false;
-    }
-
-    function loadMoreFailure (response) {
-    	TbUtils.showErrorMessage('error', response, 'No se pudo cargar mas clases.', 'Error');
-    	vm.loadingMore = false;
-    	vm.classesLoading = false;
-    }
+    classes.getAndLoad(0, vm.pageSize, vm.classes, () => { vm.classesLoading = false; });
 
 }
 
