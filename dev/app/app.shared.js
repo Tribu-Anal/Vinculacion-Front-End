b8626be20314d4aa0bae80d298396460c5732362
wupa9 <wupaz9@gmail.com>
2016-09-02 22:56:23 -0600

    Create and implement tbLoadMoreBtn directive

--- dev/app/app.shared.js
+++ dev/app/app.shared.js
@@ -1,6 +1,7 @@
 const tbButton      = require('./shared/TB-Button/tb-button.module'),
       tbHeaderbar   = require('./shared/TB-Headerbar/tb-headerbar.module'),
 	  tbLoading     = require('./shared/TB-Loading/tb-loading.module'),
+	  tbLoadMore    = require('./shared/TB-LoadMoreBtn/tb-load-more-btn.module'),
 	  tbPaginator   = require('./shared/TB-Paginator/tb-paginator.module'),
 	  tbSearchField = require('./shared/TB-SearchField/tb-search-field.module'),
 	  tbRoundBtn    = require('./shared/TB-RoundBtn/tb-round-btn.module'),
@@ -9,7 +10,7 @@ const tbButton      = require('./shared/TB-Button/tb-button.module'),
 	  tbUploadBtn   = require('./shared/TB-Upload-Btn/tb-upload-btn.module');
 
 const moduleName  = 'vinculacion.shared',
-     dependencies = [ tbButton, tbHeaderbar, tbLoading, tbPaginator, tbSearchField, 
+     dependencies = [ tbButton, tbHeaderbar, tbLoading, tbLoadMore, tbPaginator, tbSearchField, 
                       tbRoundBtn, tbSidebar, tbTable, tbUploadBtn ];
 
 angular.module(moduleName, dependencies);
