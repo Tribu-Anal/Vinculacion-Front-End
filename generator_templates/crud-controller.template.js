996a4901b3f6981271cf056da44e19f86657e520
wupa9 <wupaz9@gmail.com>
2016-09-03 02:42:16 -0600

    Finish CRUD Component Generator

--- generator_templates/crud-controller.template.js
+++ generator_templates/crud-controller.template.js
@@ -1,29 +1,37 @@
-{{properCase name}}Controller.$inject = [ 'TbUtils', '{{lowercase service}}' ];
+{{properCase name}}Controller.$inject = [ 'TbUtils', '{{camelCase service}}' ];
 
-function {{properCase name}}Controller (TbUtils, {{lowercase service}}) {
+function {{properCase name}}Controller (TbUtils, {{camelCase service}}) {
 	const vm = this;
 
-    vm.searchResults = [];
-    vm.{{lowercase modelSingular}}Obj = term => { return {  }; }; // Implement your searchObjProvider
+    vm.searchResults = []   ;
+    vm.{{camelCase modelSingular}}Obj = term => { return {  }; }; // Implement your searchObjProvider
 
-    vm.{{lowercase modelPlural}} = [];
-    vm.tableSchema = require('../../../table-schemas/{{lowercase schemaName}}-table-schema'){{if delete}}(confirmDelete){{/if}};
+    vm.{{camelCase modelPlural}} = [];
+    vm.tableSchema = require('../../../table-schemas/{{camelCase schemaName}}-table-schema'){{#if delete}}(confirmDelete){{/if}};
 
     vm.pageSize = {{pageSize}};
-    vm.get = {{lowercase service}}.getAndLoad;
-    vm.hideLoadBtn = () => vm.{{lowercase modelPlural}}.length !== vm.searchResults.length;
+    vm.get = {{camelCase service}}.getAndLoad;
+    vm.hideLoadBtn = () => vm.{{camelCase modelPlural}}.length !== vm.searchResults.length;
 
-    {{#if create}}vm.goToNew{{pascalCase modelSingular}} = () => { TbUtils.go('main.new-{{lowercase modelSingular}}'); };{{/if}}
-    {{#if update}}vm.edit{{pascalCase modelSingular}} = {{lowercase modelSingular}} => { TbUtils.go('main.edit-{{lowercase modelSingular}}', { {{lowercase modelSingular}}: btoa(JSON.stringify({{lowercase modelSingular}})) }); };{{/if}}
+    {{#if create}}vm.goToNew{{pascalCase modelSingular}} = () => { TbUtils.go('main.new-{{camelCase modelSingular}}'); };{{/if}}{{#if update}}{{#if create}}
+    vm.edit{{pascalCase modelSingular}} = {{camelCase modelSingular}} => { TbUtils.go('main.edit-{{camelCase modelSingular}}', { {{camelCase modelSingular}}: btoa(JSON.stringify({{camelCase modelSingular}})) }); };
 
-	vm.{{lowercase modelPlural}}Loading = true;
+    vm.{{camelCase modelPlural}}Loading = true;
+    {{~else~}}
+    vm.edit{{pascalCase modelSingular}} = {{camelCase modelSingular}} => { TbUtils.go('main.edit-{{camelCase modelSingular}}', { {{camelCase modelSingular}}: btoa(JSON.stringify({{camelCase modelSingular}})) }); };
 
-    {{lowercase service}}.getAndLoad(0, vm.pageSize, vm.{{lowercase modelPlural}}, () => { vm.{{lowercase modelPlural}}Loading = false; });
+    vm.{{camelCase modelPlural}}Loading = true;{{/if}}
+    {{~else}}
+    {{~#if create}}
 
-    {{#if delete}}function confirmDelete ({{lowercase modelSingular}}) {
+    vm.{{camelCase modelPlural}}Loading = true;{{else}}vm.{{camelCase modelPlural}}Loading = true;{{/if}}{{/if}}
+
+    {{camelCase service}}.getAndLoad(0, vm.pageSize, vm.{{camelCase modelPlural}}, () => { vm.{{camelCase modelPlural}}Loading = false; });{{#if delete}}
+
+    function confirmDelete ({{camelCase modelSingular}}) {
         TbUtils.confirm('Eliminar', 'Esta seguro de eliminar este dato?', result => { 
             if (result)
-                {{lowercase service}}.deleteAndNotify({{lowercase modelSingular}}, 'El dato se elimino exitosamente.');
+                {{camelCase service}}.deleteAndNotify({{camelCase modelSingular}}, 'El dato se elimino exitosamente.');
         });
     }{{/if}}
 
