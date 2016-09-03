002724786c5204f4ec6948d18b3d0f5fb2760ca2
wupa9 <wupaz9@gmail.com>
2016-09-03 15:01:07 -0600

    Update search field in projects view

--- dev/app/components/main/projects/projects-bundle.module.js
+++
@@ -1,9 +0,0 @@
-const projects = require('./projects.module'),
-      dialogs  = require('./dialogs/dialogs-bundle.module');
-
-const moduleName   = 'projects-bundle.component',
-      dependencies = [ projects, dialogs ];
-
-angular.module(moduleName, dependencies);
-
-module.exports = moduleName;
