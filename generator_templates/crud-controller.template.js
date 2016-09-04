ee17104bd6fb39a7712ef8f59dd28df1522e7b3b
wupa9 <wupaz9@gmail.com>
2016-09-03 18:44:31 -0600

    Update generators

--- generator_templates/crud-controller.template.js
+++ generator_templates/crud-controller.template.js
@@ -3,30 +3,38 @@
 function {{properCase name}}Controller (TbUtils, {{camelCase service}}) {
 	const vm = this;
 
+    // TB-SearchField Params
+
     vm.searchResults = []   ;
     vm.{{camelCase modelSingular}}Obj = term => { return {  }; }; // Implement your searchObjProvider
+
+    // TB-Table Params
 
     vm.{{camelCase modelPlural}} = [];
     vm.tableSchema = require('../../../table-schemas/{{camelCase schemaName}}-table-schema'){{#if delete}}(confirmDelete){{/if}};
 
+    // TB-LoadMoreBtn Params
+
     vm.pageSize = {{pageSize}};
     vm.get = {{camelCase service}}.getWithPagination; // Modify this to set the pagination API call you need
+    vm.getAll = {{camelCase service}}.get; // Modify this to set the correct GET for all model data
     vm.hideLoadBtn = () => vm.{{camelCase modelPlural}}.length !== vm.searchResults.length;
 
     {{#if create}}vm.goToNew{{pascalCase modelSingular}} = () => { TbUtils.go('main.new-{{camelCase modelSingular}}'); };{{/if}}{{#if update}}{{#if create}}
     vm.edit{{pascalCase modelSingular}} = {{camelCase modelSingular}} => { TbUtils.go('main.edit-{{camelCase modelSingular}}', { {{camelCase modelSingular}}: btoa(JSON.stringify({{camelCase modelSingular}})) }); };
 
-    vm.{{camelCase modelPlural}}Loading = true;
+    vm.loading = true;
     {{~else~}}
     vm.edit{{pascalCase modelSingular}} = {{camelCase modelSingular}} => { TbUtils.go('main.edit-{{camelCase modelSingular}}', { {{camelCase modelSingular}}: btoa(JSON.stringify({{camelCase modelSingular}})) }); };
 
-    vm.{{camelCase modelPlural}}Loading = true;{{/if}}
+    vm.loading = true;{{/if}}
     {{~else}}
     {{~#if create}}
 
-    vm.{{camelCase modelPlural}}Loading = true;{{else}}vm.{{camelCase modelPlural}}Loading = true;{{/if}}{{/if}}
+    vm.loading = true;{{else}}vm.loading = true;{{/if}}{{/if}}
 
-    TbUtils.getAndLoad({{camelCase service}}.get, vm.{{camelCase modelPlural}}, () => { vm.{{camelCase modelPlural}}Loading = false; }, 0, vm.pageSize);{{#if delete}}
+    // You can modify the first argument to set the correct API call for your model's initial value
+    TbUtils.getAndLoad({{camelCase service}}.get, vm.{{camelCase modelPlural}}, () => { vm.loading = false; }, 0, vm.pageSize);{{#if delete}}
 
     // Feel free to modify the confirm text
     // Also, set the correct DELETE API call
