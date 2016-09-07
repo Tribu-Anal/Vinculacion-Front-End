1b6377df729b5171f901fd64ff499a9a2fa80bc4
wupa9 <wupaz9@gmail.com>
2016-09-06 19:39:00 -0600

    Modify table schema for users

--- dev/app/table-schemas/users-table-schema.js
+++ dev/app/table-schemas/users-table-schema.js
@@ -1,9 +1,27 @@
-module.exports = {
-	headers: [ 'Numero de Cuenta', 'Nombre', 'Correo', 'Carrera' ],
-	rows: [
-		{ type: 'label', props: { text: obj => obj.AccountId }  },
-		{ type: 'label', props: { text: obj => obj.Name }  },
-		{ type: 'label', props: { text: obj => obj.Email ? obj.Email : 'N/A' }  },
-		{ type: 'label', props: { text: obj => obj.Major ? obj.Major.Name : 'N/A' }  }
-	]
+module.exports = userType => {
+	let schema = {
+		headers: [ 'Numero de Cuenta', 'Nombre', 'Correo', 'Campus', 'Activado' ],
+		rows: [
+			{ type: 'label', props: { text: obj => obj.AccountId }  },
+			{ type: 'label', props: { text: obj => obj.Name }  },
+			{ type: 'label', props: { text: obj => obj.Email ? obj.Email : 'N/A' }  },
+			{ type: 'label', props: { text: obj => obj.Campus } },
+			{ type: 'icon',  props: { iconClass: obj => obj.Status.Active ? 
+				'glyphicon glyphicon-ok' : 'glyphicon glyphicon-remove',  fontSize: 24 } }
+		]
+	};
+	
+	if (userType === 'student') {
+		schema.headers.splice(3, 0, 'Carrera');
+		schema.headers.splice(5, 0, 'Finiquiteado');
+
+		schema.rows.splice(3, 0, 
+			{ type: 'label', props: { text: obj => obj.Major ? obj.Major.Name : 'N/A' }  });
+
+		schema.rows.splice(5, 0, 
+			{ type: 'icon',  props: { iconClass: obj => obj.Finiquiteado ? 
+				'glyphicon glyphicon-ok' : 'glyphicon glyphicon-remove',  fontSize: 24 } });
+	}
+
+	return schema;
 };
