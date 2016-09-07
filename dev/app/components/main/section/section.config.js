972914b3c23f93ae7c4e3d593758a71937221429
wupa9 <wupaz9@gmail.com>
2016-09-07 02:31:20 -0600

    Fix stylsheet

--- dev/app/components/main/section/section.config.js
+++ dev/app/components/main/section/section.config.js
@@ -4,7 +4,7 @@ function config ($stateProvider) {
 
 	$stateProvider
 		.state('main.section', {
-            url: '/secciones/{sectionId}',
+            url: '/secciones/?sectionId',
             templateUrl: 'templates/components/main/section/section.html',
             controller: "SectionController as vm",
 			onEnter: onStateEnter
