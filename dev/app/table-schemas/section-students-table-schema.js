86f276f06d33d09fa1b028accee7e8b6c7771345
wupa9 <wupaz9@gmail.com>
2016-09-09 03:04:09 -0600

    Remove delete from schema

--- dev/app/table-schemas/section-students-table-schema.js
+++ dev/app/table-schemas/section-students-table-schema.js
@@ -1,15 +1,7 @@
-module.exports = btnClick => {
-	const model = {
-		headers: [ 'Numero de Cuenta', 'Nombre', '' ],
-		rows: [
-			{ type: 'label', props: { text: obj => obj.AccountId }  },
-			{ type: 'label', props: { text: obj => obj.Name }  },
-			{ type: 'button', props: { icon: 'glyphicon glyphicon-trash', 
-			                           tooltip: 'Borrar Alumno', 
-			                           onClick: btnClick } 
-			}
-		]
-	};
-
-	return model;
+module.exports = {
+	headers: [ 'Numero de Cuenta', 'Nombre' ],
+	rows: [
+		{ type: 'label', props: { text: obj => obj.AccountId }  },
+		{ type: 'label', props: { text: obj => obj.Name }  }
+	]
 };
